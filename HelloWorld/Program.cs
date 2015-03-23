e1675e977fcd923999b855a9370c897c72c69673
Jon Reed <mr.is@outlook.com>
2015-03-23 13:38:38 -0400

    Step 4
    Added Comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;   //Using statements are used to reference built it library with in VS. this gives our program access to those separate librarys of classes and methods
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace HelloWorld //namespace hold a collection of classes and methods. where our program lives.
 {
-    class Program
+    class Program //class holds or methods that live in the class and also names the class which can be used to access the methods within the class "house".
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//method the uses the keyword "main" is where the programs execution would start or begin.
         {
+
         }
     }
 }
